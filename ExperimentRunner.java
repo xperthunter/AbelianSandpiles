1059ca0ae8535c4971acab3e8e66ba0196b00db0
Keith Fraga <k.fraga@hvcc.edu>
2026-02-04 13:45:56 -0500

    formatted output a bit more

--- ExperimentRunner.java
+++ ExperimentRunner.java
@@ -2,27 +2,27 @@ import java.util.*;
 
 public class ExperimentRunner {
     public static void main(String[] args) {
-        final int DROPS = 1000000;
+        final int DROPS = 100000;
         List<Integer> avalancheSizes;
-        int[] GridSizes = {16, 32, 64, 128};
+        int[] GridSizes = {8, 16, 32, 64, 128, 256};
         long seed;
 
         System.out.println("=== Experiment Runner ===");
 
         for (int n : GridSizes) {
-            System.out.println("> " + n);
+            System.out.println("\t> " + n);
             seed = System.nanoTime();
             SandpileSimulation.Sandpile sp = new SandpileSimulation.Sandpile(n, seed);
 
             List<Integer> avalanches = sp.runDrops(DROPS);
 
             SandpileSimulation.Stats st = new SandpileSimulation.Stats(avalanches);
-            System.out.printf("Avalance sizes: count=%d, min=%d, max=%d, mean=%.4f%n",
+            System.out.printf("\t\tAvalance sizes: count=%d, min=%d, max=%d, mean=%.4f%n",
             st.count, st.min, st.max, st.mean);
 
             // fraction of zero avalanche  drops (no toppling)
             long zeros = avalanches.stream().filter(v -> v == 0).count();
-            System.out.printf("Zero-size avalanches: %d (%.2f%%)%n",
+            System.out.printf("\t\tZero-size avalanches: %d (%.2f%%)%n",
                 zeros, 100.0 * zeros / Math.max(1, st.count));
         }
     }
