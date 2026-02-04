b9b60359ddc19b9dfb01077f1a8c958f19573581
Keith Fraga <k.fraga@hvcc.edu>
2026-02-04 12:31:35 -0500

    file renaming?
    does the file naming get fixed?

---
+++ ExperimentRunner.java
@@ -0,0 +1,6 @@
+public class ExperimentRunner {
+    public static void main(String[] args) {
+    System.out.println("=== Experiment Runner ===");
+    SandpileSimulation sp = new SandpileSimulation();
+    }
+}
